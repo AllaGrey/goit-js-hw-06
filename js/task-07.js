dfd022dab9710077567aad72395113765abbb5f0
AllaGrey <alla.basanko2012@gmail.com>
2023-01-16 17:46:37 +0200

    update 7 task

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,5 @@
-
 const inputEl = document.querySelector("#font-size-control");
 const spanEl = document.querySelector("#text");
-
-console.log(inputEl.value);
-console.log(spanEl);
 
 inputEl.addEventListener("input", (event) => {
   const inputFontSize = event.currentTarget.value;
